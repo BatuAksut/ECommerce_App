0461387a1bb93dd36bdde608f6077e5c849f26eb
Batuhan Aksüt <batuhanaksut@hotmail.com>
2025-12-02 15:53:54 +0300

    feat: Enhance delete directive with controller input and alertify service integration

--- ECommerceClient/src/app/directives/admin/delete.directive.ts
+++ ECommerceClient/src/app/directives/admin/delete.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, input, Output, Renderer2 } from '@angular/core';
-import { ProductService } from '../../services/common/models/product.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
+import { HttpClientService } from '../../services/common/http-client.service';
+import { AlertifyService, MessageType, Position } from '../../services/admin/alertify.service';
 declare var $: any;
 @Directive({
   selector: '[appDelete]'
@@ -10,8 +11,9 @@ export class DeleteDirective {
 
   constructor(private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
-    private dialog:MatDialog
+    private httpClientService: HttpClientService,
+    private dialog:MatDialog,
+    private alertify:AlertifyService
   ) {
 
     const img = this._renderer.createElement("img")
@@ -21,6 +23,7 @@ export class DeleteDirective {
   }
 
 @Input() id!:string;
+@Input() controller!:string;
 
 @Output() callback:EventEmitter<any> = new EventEmitter()
 
@@ -28,10 +31,16 @@ export class DeleteDirective {
   async onclick() {
     this.openDialog(async ()=>{
     const td:HTMLTableCellElement=this.element.nativeElement
-    await this.productService.delete(this.id)
+    this.httpClientService.delete({
+      controller:this.controller
+    },this.id).subscribe()
+    
     $(td.parentElement).fadeOut(2000,()=>{
       this.callback.emit()
     })
+    this.alertify.message("Item successfully deleted.",{messageType:MessageType.Success,
+        position:Position.TopRight})
+        
   })
     
   }
